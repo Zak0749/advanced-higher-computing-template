71178b59f3c3a7477ef840316adc0cdb6b0b2182
Zak Grivell <89150707+Zak0749@users.noreply.github.com>
2024-08-27 22:23:09 +0000

    Fin

--- index.php
+++ index.php
@@ -1,27 +1,45 @@
 <?php
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+$db = new SQLite3('./database.db');
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
+$db->exec("CREATE TABLE IF NOT EXISTS Names (name varchar(16))");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$name = $_POST["name"];
+
+	$sql = "INSERT INTO Names (name) VALUES (:name)";
+	$stmt = $db->prepare($sql);
+	$stmt->bindParam(':name', $name);
+	$stmt->execute();}	
+
+$rows = $db->query("SELECT * FROM Names");
 
 ?>
 
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>AH Computing Example - Names</title>
+</head>
+<body>
+	<h1>Add a name</h1>
+
+	<form action="/" method="post">
+		<input type="text" name="name" minlength="3" maxlength="16">
+
+		<input type="submit">
+	</form>
+
+	<h1>Names</h1>
+
+	<?php
+
+	while ($name = $rows->fetchArray()) {
+		echo $name["name"] . "<br>";
+	}
+
+	?>
+</body>
 </html>
---
+++ readme.md
@@ -0,0 +1,9 @@
+## To run 
+
+CMD + Shift + P 
+
+Type in Run Task
+
+Select PHP
+
+or type 'php -S localhost:8080' into the
